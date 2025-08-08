113bbeee199b0346df35a376db386dc90b1dd253
maheb05 <mahaboobmunna14@gmail.com>
2025-08-08 13:11:36 +0530

    changes

--- carapi/src/main/java/com/carapi/CarapiApplication.java
+++ carapi/src/main/java/com/carapi/CarapiApplication.java
@@ -2,11 +2,19 @@ package com.carapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
 public class CarapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarapiApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello(){
+		return "Hello, welcome to the Car API!";
 	}
 }
